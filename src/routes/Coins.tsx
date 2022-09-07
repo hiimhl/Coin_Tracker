99b9122095a32e3a62abc3f4ae97573404bd96bf
hlin <gywjd5126@gmail.com>
2022-09-07 11:02:58 +0900

    home - fetch data, show list

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,7 +1,95 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+const Header = styled.header`
+  height: 10vh;
+  display: felx;
+  justify-content: center;
+  align-items: center;
+`;
+const CoinsList = styled.ul``;
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  font-weight: 600;
+  margin-bottom: 10px;
+  border-radius: 15px;
+
+  a {
+    padding: 20px;
+    transition: color 0.2s ease-in;
+    display: block;
+    //글씨 밖까지 클릭이 됨
+  }
+
+  &:hover {
+    a {
+      // Link = anchor , Link들은 다 a 태그로 바뀔것.
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
+const Title = styled.h1`
+  font-size: 48px;
+  font-weight: 600;
+  font-style: italic;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  font-size: 25px;
+  display: block;
+`;
+
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  in_new: boolean;
+  in_active: boolean;
+  type: string;
+}
 function Coins() {
-  return <h1>Coins</h1>;
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  // fetch coins
+  useEffect(() => {
+    (async () => {
+      // 바로 실행되는 Fn. 따로 실행시킬 필요 x
+      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await response.json();
+      setCoins(json.slice(0, 100));
+      setLoading(false);
+    })();
+  }, []);
+
+  return (
+    <Container>
+      <Header>
+        <Title>Coins</Title>
+      </Header>
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <CoinsList>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+            </Coin>
+          ))}
+        </CoinsList>
+      )}
+    </Container>
+  );
 }
 
 export default Coins;
--- src/theme.ts
+++ src/theme.ts
@@ -2,7 +2,7 @@ import { DefaultTheme } from "styled-components";
 // theme 타입을 정의한 파일.
 
 export const theme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
-  btnColor: "tomato",
+  bgColor: "#1b4965",
+  textColor: "#cae9ff",
+  accentColor: "#d9ed92", //2ec4b6
 };
