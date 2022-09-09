d2aa3137311147fb09edb996f816edd5a888bf7c
hlin <gywjd5126@gmail.com>
2022-09-09 22:40:26 +0900

    e

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import Coins from "./routes/Coins";
 import Coin from "./routes/Coin";
 
 function Router() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/:coinId/*" element={<Coin />} />
         <Route path="/" element={<Coins />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
